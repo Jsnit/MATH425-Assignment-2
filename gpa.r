6273d0b9414a64cb61ae183dce05361806aa34a6
Jsnit <jrsnitbro@gmail.com>
2024-09-30 22:12:37 -0700

    Updated with completed docs

--- gpa.r
+++ gpa.r
@@ -3,12 +3,27 @@
 #setwd("~/Desktop/School/Math 425 Applied Stats Models/MATH425-Assignment-2")
 #******************************************************************************
 
-gpa_act_data = read.csv("data/gpa_act_data.csv", header=T)
+data = read.csv("data/gpa_act_data.csv", header=T)
 
 # x = ACT SCORE
-x = gpa_act_data$act
+x = data$act
 # y = GPA
-y = gpa_act_data$gpa
+y = data$gpa
 
 # Set graph to 1x1
 par(mfrow = c(1, 1))
+
+
+model=lm(y~x,data=data)
+anova(model)
+#For the F test we look at the P-value of V2. Since it is less than 0.05, we reject 
+#the null hypothesis and declare that there is a correlation
+
+summary(model)
+r.squared=summary(model)$r.squared
+correlation.coefficient=sqrt(r.squared)
+r.squared
+#0.0726
+correlation.coefficient
+cor(data)
+#0.2694
--- musclemass.r
+++ musclemass.r
@@ -11,4 +11,45 @@ x = muscle_age_data$age
 y = muscle_age_data$musclemass
 
 # Set graph to 1x1
-par(mfrow = c(1, 1))
+#par(mfrow = c(1, 1))
+
+model = lm(y~x)
+summary(model)
+
+p_value <- summary(model)$coefficients[2, 4]
+
+print(paste("P-value:", p_value))
+
+# 95% confidence interval for the slope
+confint(model, level = 0.95)
+# Extract the slope
+slope <- coef(model)[2]
+
+# Print slope
+print(paste("Estimated decrease in muscle mass for a one-year increase in age:", slope))
+if(p_value < 0.05) {
+  print("Reject the null hypothesis. There is significant evidence of a negative linear relationship between age and muscle mass.")
+} else {
+  print("Fail to reject the null hypothesis. There is not enough evidence to conclude a significant negative relationship between age and muscle mass.")
+}
+
+
+# Find SSE & SSR for R-Squared value
+sse <- sum((fitted(model) - muscle_age_data$musclemass)^2)
+sse
+# 3874.77
+
+#find ssr
+ssr <- sum((fitted(model) - mean(muscle_age_data$age))^2)
+ssr
+# 49077.5
+
+# find sst
+#find sst
+sst <- ssr + sse
+sst
+
+# R-Squared = 1-SSE/SST
+r2 = 1-(sse/sst)
+r2
+# 0.9268
